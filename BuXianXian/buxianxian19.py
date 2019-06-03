b0dfc5b3ba69a628fd81f86c5332cc1e5d9f38c3
ZhangYajun12138 <18521064458@163.com>
2019-06-03 14:34:02 +0800

    在屏幕上显示跑马灯文字

--- BuXianXian/buxianxian19.py
+++ BuXianXian/buxianxian19.py
@@ -1,1 +1,18 @@
 # coding:utf-8
+# 在屏幕上显示跑马灯文字
+
+import os
+import time
+
+def main():
+    content = "北京欢迎你为你开天辟地.........."
+
+    while True:
+        # 清理屏幕上的输出
+        os.system("cls")
+        print(content)
+        time.sleep(0.1)
+        content = content[1:] + content[0]
+
+if __name__ == '__main__':
+    main()
